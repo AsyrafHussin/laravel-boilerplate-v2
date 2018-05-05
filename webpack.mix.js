d6e664c1f5c55c486780ba19e56928eea18c95d7
Asyraf Hussin <asyrafhussin4@gmail.com>
2018-05-05 15:29:17 +0800

    Add cssnano

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,8 @@ let mix = require('laravel-mix');
  mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css').options({
         postCss: [ 
-            require('autoprefixer') 
+            require('autoprefixer'),
+            require('cssnano'),
         ]
     })
     .browserSync('127.0.0.1:8000')
