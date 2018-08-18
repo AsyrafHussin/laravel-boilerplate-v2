debadfe91b09c4b1869c784a79bfe74ce9503b30
Asyraf Hussin <asyrafhussin4@gmail.com>
2018-08-19 07:21:22 +0800

    Refactor javascript

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,1 +1,11 @@
+import jquery from 'jquery';
+
+window.jQuery = jquery;
+window.$ = jquery;
+window.jquery = jquery;
+
 require('bootstrap');
+
+jQuery(document).ready(function($) {
+  // script here
+});
