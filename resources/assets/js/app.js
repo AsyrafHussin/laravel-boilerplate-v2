f5df9c45ef398e76721d2ecc149f99ed8608097c
Asyraf Hussin <asyrafhussin4@gmail.com>
2018-09-10 13:56:49 +0800

    Use sweetalert2 from npm

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,6 +4,8 @@ window.jQuery = jquery;
 window.$ = jquery;
 window.jquery = jquery;
 import 'bootstrap';
+import swal from 'sweetalert2';
+window.swal = swal;
 
 // import module
 import Scroll from './modules/scroll.js';
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,9 +11,6 @@
     <h1>Laravel boilerplate</h1>
 
     <script src="{{ mix('js/app.js') }}"></script>
-    <script src="https://unpkg.com/sweetalert2@7.3.4/dist/sweetalert2.all.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
-
     @include('errors.messages')
 </body>
 </html>
