f3635ee3a13edbf50d7aeddcb0eecda374a10b75
Asyraf Hussin <asyrafhussin4@gmail.com>
2018-09-10 13:09:24 +0800

    Test compile

--- public/js/app.js
+++ public/js/app.js
@@ -17017,7 +17017,7 @@ var Scroll = function () {
 /***/ "./resources/assets/sass/app.scss":
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: \n  @if $debug-mode{\n     ^\n      Undefined variable: \"$debug-mode\".\n      in /Users/asyrafhussin/Desktop/MyWork/Laravel/laravel-boilerplate-v2/resources/assets/sass/themes/_default.scss (line 2, column 7)\n    at runLoaders (/Users/asyrafhussin/Desktop/MyWork/Laravel/laravel-boilerplate-v2/node_modules/webpack/lib/NormalModule.js:195:19)\n    at /Users/asyrafhussin/Desktop/MyWork/Laravel/laravel-boilerplate-v2/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /Users/asyrafhussin/Desktop/MyWork/Laravel/laravel-boilerplate-v2/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/Users/asyrafhussin/Desktop/MyWork/Laravel/laravel-boilerplate-v2/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/Users/asyrafhussin/Desktop/MyWork/Laravel/laravel-boilerplate-v2/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.done [as callback] (/Users/asyrafhussin/Desktop/MyWork/Laravel/laravel-boilerplate-v2/node_modules/neo-async/async.js:7974:18)\n    at options.error (/Users/asyrafhussin/Desktop/MyWork/Laravel/laravel-boilerplate-v2/node_modules/node-sass/lib/index.js:294:32)");
+// removed by extract-text-webpack-plugin
 
 /***/ }),
 
