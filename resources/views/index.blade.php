7fda4fc1617672db4fce026d8fd80153af54a39e
Asyraf Hussin <asyrafhussin4@gmail.com>
2018-09-10 13:52:25 +0800

    Remove jqeury cdn

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,6 @@
 <body>
     <h1>Laravel boilerplate</h1>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="{{ mix('js/app.js') }}"></script>
     <script src="https://unpkg.com/sweetalert2@7.3.4/dist/sweetalert2.all.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/core-js/2.4.1/core.js"></script>
