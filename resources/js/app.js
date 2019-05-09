c492353c871aa4fa3622d0cd8668ecfeb388a07d
Asyraf Hussin <asyrafhussin4@gmail.com>
2019-05-10 06:05:13 +0800

    Add boostrap datepicker

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,10 +3,17 @@ window.jQuery = window.$ = require('jquery');
 import 'bootstrap';
 import Swal from 'sweetalert2';
 window.Swal = Swal;
+import 'bootstrap-datepicker';
 
 // import module
 import Scroll from './modules/scroll.js';
 
 $(document).ready(function() {
   Scroll.init();
+
+  $('.datepicker').datepicker({
+    format: "dd/mm/yyyy",
+    autoclose: true,
+    startDate: new Date()
+  });
 });
