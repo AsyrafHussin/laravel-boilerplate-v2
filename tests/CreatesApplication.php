1bfdde99de0472082fb91aa920a685d0fb57c58a
Asyraf Hussin <asyrafhussin4@gmail.com>
2019-05-19 03:21:23 +0800

    Update CreatesApplication test

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -3,7 +3,6 @@
 namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
-use Illuminate\Support\Facades\Hash;
 
 trait CreatesApplication
 {
@@ -15,10 +14,7 @@ trait CreatesApplication
     public function createApplication()
     {
         $app = require __DIR__ . '/../bootstrap/app.php';
-
         $app->make(Kernel::class)->bootstrap();
-
-        Hash::driver('bcrypt')->setRounds(4);
 
         return $app;
     }
