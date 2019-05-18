ce5babffcba27534699e590f7c873e24aa89a5bc
Asyraf Hussin <asyrafhussin4@gmail.com>
2019-05-19 03:39:09 +0800

    Fix bouncer

--- app/User.php
+++ app/User.php
@@ -4,10 +4,13 @@ namespace App;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Silber\Bouncer\Database\HasRolesAndAbilities;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use HasRolesAndAbilities;
+
     /**
      * The attributes that are mass assignable.
      *
