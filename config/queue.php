721d75e8b5b65a5de39727c943e6a6cfbf7c669e
Asyraf Hussin <asyrafhussin4@gmail.com>
2019-05-19 03:07:59 +0800

    Update config/queue.php

--- config/queue.php
+++ config/queue.php
@@ -11,9 +11,7 @@ return [
     | syntax for every one. Here you may define a default connection.
     |
     */
-
-    'default' => env('QUEUE_DRIVER', 'sync'),
-
+    'default' => env('QUEUE_CONNECTION', 'sync'),
     /*
     |--------------------------------------------------------------------------
     | Queue Connections
@@ -26,44 +24,39 @@ return [
     | Drivers: "sync", "database", "beanstalkd", "sqs", "redis", "null"
     |
     */
-
     'connections' => [
         'sync' => [
             'driver' => 'sync',
         ],
-
         'database' => [
             'driver'      => 'database',
             'table'       => 'jobs',
             'queue'       => 'default',
             'retry_after' => 90,
         ],
-
         'beanstalkd' => [
             'driver'      => 'beanstalkd',
             'host'        => 'localhost',
             'queue'       => 'default',
             'retry_after' => 90,
+            'block_for'   => 0,
         ],
-
         'sqs' => [
             'driver' => 'sqs',
-            'key'    => env('SQS_KEY', 'your-public-key'),
-            'secret' => env('SQS_SECRET', 'your-secret-key'),
+            'key'    => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'prefix' => env('SQS_PREFIX', 'https://sqs.us-east-1.amazonaws.com/your-account-id'),
             'queue'  => env('SQS_QUEUE', 'your-queue-name'),
-            'region' => env('SQS_REGION', 'us-east-1'),
+            'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
         ],
-
         'redis' => [
             'driver'      => 'redis',
             'connection'  => 'default',
-            'queue'       => 'default',
+            'queue'       => env('REDIS_QUEUE', 'default'),
             'retry_after' => 90,
             'block_for'   => null,
         ],
     ],
-
     /*
     |--------------------------------------------------------------------------
     | Failed Queue Jobs
@@ -74,7 +67,6 @@ return [
     | have failed. You may change them to any database / table you wish.
     |
     */
-
     'failed' => [
         'database' => env('DB_CONNECTION', 'mysql'),
         'table'    => 'failed_jobs',
