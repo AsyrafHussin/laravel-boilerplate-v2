1f693cfb7da6a62580b7160e20c9e1f995f9642b
Asyraf Hussin <asyrafhussin4@gmail.com>
2019-05-19 03:02:20 +0800

    Update config/auth.php

--- config/auth.php
+++ config/auth.php
@@ -39,10 +39,10 @@ return [
             'driver'   => 'session',
             'provider' => 'users',
         ],
-
         'api' => [
             'driver'   => 'token',
             'provider' => 'users',
+            'hash'     => false,
         ],
     ],
 
