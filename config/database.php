51df2db0c1dc055c87839762aa97c48ffba637f6
Asyraf Hussin <asyrafhussin4@gmail.com>
2019-07-20 17:20:20 +0800

    Update laravel core

--- config/database.php
+++ config/database.php
@@ -106,7 +106,7 @@ return [
         'client' => env('REDIS_CLIENT', 'predis'),
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'predis'),
-            'prefix' => Str::slug(env('APP_NAME', 'laravel'), '_').'_database_',
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
         'default' => [
             'url' => env('REDIS_URL'),
