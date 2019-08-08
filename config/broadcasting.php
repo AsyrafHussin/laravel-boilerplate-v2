a094c1cf39b2c1b4b6b433878f24b7ee43968aef
Asyraf Hussin <asyrafhussin4@gmail.com>
2019-08-08 20:09:43 +0800

    Run csfix

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -35,7 +35,7 @@ return [
             'app_id'  => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster'   => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
+                'useTLS'    => true,
             ],
         ],
 
