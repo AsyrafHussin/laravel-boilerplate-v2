468e303f3d84221956afcab31db1c65a5defe869
Asyraf Hussin <asyrafhussin4@gmail.com>
2019-10-24 17:40:06 +0800

    Add model and migration for email verify

---
+++ app/EmailVerify.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EmailVerify extends Model
+{
+	protected $guarded = [];
+	protected $table = 'email_verifies';
+
+	protected $fillable = [
+		'email', 'token', 'created_at',
+	];
+
+	public $timestamps = false;
+
+	protected static function boot() {
+		parent::boot();
+
+		static::creating(function ($query) {
+			$query->token = generateToken(20);
+		});
+	}
+}
---
+++ database/migrations/2019_10_24_173914_create_email_verifies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmailVerifiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('email_verifies', function (Blueprint $table) {
+            $table->string('email')->index();
+            $table->longText('token');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('email_verifies');
+    }
+}
