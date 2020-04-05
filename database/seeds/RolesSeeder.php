5c2aab37aadc37fa32ef6c6e0a5316f5ed3f04c2
Asyraf Hussin <asyrafhussin4@gmail.com>
2020-04-05 08:18:46 +0800

    Update core

--- database/seeds/RolesSeeder.php
+++ database/seeds/RolesSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class RolesTableSeeder extends Seeder
+class RolesSeeder extends Seeder
 {
     /**
      * Run the database seeds.
--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -3,7 +3,7 @@
 use App\User;
 use Illuminate\Database\Seeder;
 
-class UsersTableSeeder extends Seeder
+class UsersSeeder extends Seeder
 {
     /**
      * Run the database seeds.
