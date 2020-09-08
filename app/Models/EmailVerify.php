e2d35f393ded5d1ec8ebb93032960b45b47a4661
Asyraf Hussin <asyrafhussin4@gmail.com>
2020-09-09 03:05:16 +0800

    Add comments

--- app/Models/EmailVerify.php
+++ app/Models/EmailVerify.php
@@ -6,14 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class EmailVerify extends Model
 {
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
     protected $table = 'email_verifies';
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'email', 'token', 'created_at',
     ];
 
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
     public $timestamps = false;
 
+    /**
+     * The "boot" method of the model.
+     *
+     * @return void
+     */
     protected static function boot()
     {
         parent::boot();
