52f98d24918642b7de974b9fbd17e24797b2c69d
Asyraf Hussin <asyrafhussin4@gmail.com>
2020-09-10 11:01:06 +0800

    Update Helpers

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -4,65 +4,62 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Pagination\Paginator;
 
-/*
- * Generate token.
- *
- * @param  int    - length token that want to generate
- *
- * @return string - token
- */
 if (! function_exists('generateToken')) {
+    /**
+     * Generate token.
+     *
+     * @param int - length token that want to generate
+     *
+     * @return string - token
+     */
     function generateToken($length)
     {
         return bin2hex(random_bytes($length));
     }
 }
 
-/*
- * Save image
- *
- * @param object - image, string - folder name
- * @return string - image upload path
- */
 if (! function_exists('saveImg')) {
     /**
-     * @param $img
+     * Save image.
+     *
+     * @param object - image
+     * @param string - folder name
+     *
+     * @return string - image upload path
      */
     function saveImg($img, $folder = 'images')
     {
-        return 'storage/' . $img->store($folder);
+        return 'storage/'.$img->store($folder);
     }
 }
 
-/*
- * Remove image
- *
- * @param string - image path
- * @return void
- */
 if (! function_exists('removeImg')) {
     /**
-     * @param $img
+     * Remove image.
+     *
+     * @param string - image path
+     *
+     * @return void
      */
     function removeImg($img)
     {
         $img = str_replace('storage/', '', $img);
-        if (file_exists(storage_path('app/public/') . $img)) {
-            unlink(storage_path('app/public/') . $img);
+        if (file_exists(storage_path('app/public/').$img)) {
+            unlink(storage_path('app/public/').$img);
         }
     }
 }
 
-/*
- * Custom paginate for collection
- *
- * @return void
- */
 if (! function_exists('customPaginate')) {
+    /**
+     * Custom paginate for collection.
+     *
+     * @return Illuminate\Pagination\LengthAwarePaginator
+     */
     function customPaginate($items, $path = null, $perPage = 20, $page = null)
     {
-        $path  = $path ? $path : Paginator::resolveCurrentPage();
-        $page  = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+        $path = $path ? $path : Paginator::resolveCurrentPage();
+        $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
         $items = $items instanceof Collection ? $items : Collection::make($items);
 
         return new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, [
