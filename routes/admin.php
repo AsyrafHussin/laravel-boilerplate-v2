5fda59f21d1b18f03affe632b328b75efdab010f
Asyraf Hussin <asyrafhussin4@gmail.com>
2020-09-10 14:38:57 +0800

    Update admin route

--- routes/admin.php
+++ routes/admin.php
@@ -10,7 +10,8 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register admin routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" , "auth  and "admin" middleware group. Now create something great!
+| contains the "web" , "auth  and "admin" middleware group. Route name start
+| with "admin." and Route prefix start with "admin". Now create something great!
 |
 */
 
