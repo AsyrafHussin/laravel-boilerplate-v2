2ae44930ce2ebc4c7f44f3e46d38eba82c7bf996
Asyraf Hussin <asyrafhussin4@gmail.com>
2020-10-07 18:14:53 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 rm -rf ./.git && rm -rf ./README.md
-composer install -vvv --color=always
+composer install -vvv
 cp .env.example .env
 php artisan key:generate --ansi
 php artisan config:cache
 php artisan config:clear
 php artisan storage:link
-npm install --color=always
+npm install
 npm run dev
 echo "Successfully setup!"
 rm -rf ./setup.sh
