1574621d559ab3394ae0765f3fa8b0ca3b445cfe
Asyraf Hussin <asyrafhussin4@gmail.com>
2021-02-10 07:31:54 +0800

    Add unverified state to UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -33,6 +33,20 @@ class UserFactory extends Factory
     }
 
     /**
+     * Define the model's unverified state.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function unverified()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'email_verified_at' => null,
+            ];
+        });
+    }
+
+    /**
      * Define admin default state.
      *
      * @return array
