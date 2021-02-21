147a2a307916398c76c4395711ecd725c794463c
Asyraf Hussin <asyrafhussin4@gmail.com>
2021-02-22 03:49:45 +0800

    Add ckeditor

--- resources/js/modules/vendor.js
+++ resources/js/modules/vendor.js
@@ -1,21 +1,45 @@
-import jquery from 'jquery';
+import jquery from "jquery";
 window.jQuery = jquery;
-import 'bootstrap-datepicker';
+import "bootstrap-datepicker";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 /* Initialize library */
 class Vendor {
-  static init() {
-    // init datepicker
-    $('.datepicker-nopast').datepicker({
-      format: "dd/mm/yyyy",
-      autoclose: true,
-      startDate: new Date()
-    });
-    $('.datepicker').datepicker({
-      format: "dd/mm/yyyy",
-      autoclose: true,
-    });
-  }
+    static init() {
+        // init datepicker
+        $(".datepicker-nopast").datepicker({
+            format: "dd/mm/yyyy",
+            autoclose: true,
+            startDate: new Date(),
+        });
+        $(".datepicker").datepicker({
+            format: "dd/mm/yyyy",
+            autoclose: true,
+        });
+
+        // init ckeditor
+        if (document.getElementsByClassName("editor").length > 0) {
+            ClassicEditor.create(document.querySelector(".editor"), {
+                removePlugins: [
+                    "CKFinderUploadAdapter",
+                    "CKFinder",
+                    "EasyImage",
+                    "Image",
+                    "ImageCaption",
+                    "ImageStyle",
+                    "ImageToolbar",
+                    "ImageUpload",
+                    "MediaEmbed",
+                ],
+            })
+                .then((editor) => {
+                    console.log(editor);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }
+    }
 }
 
 export default Vendor;
