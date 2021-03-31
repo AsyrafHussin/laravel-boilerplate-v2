66cbb41ef219652668521047bd76e3b6ec4b793d
Asyraf Hussin <asyrafhussin4@gmail.com>
2021-03-31 19:40:54 +0800

    Rename test methods

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,3 +1,4 @@
+
 <?php
 
 namespace Tests\Feature;
@@ -12,7 +13,7 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function test_the_application_returns_a_successful_response()
+    public function test_example()
     {
         $response = $this->get('/');
 
